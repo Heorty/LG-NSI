bf62a80d341fb6ab1fc22c105f52252e9efa9ded
Julien THILLARD <julien.thillard38@gmail.com>
2021-11-22 15:20:26 +0100

    v1.0

--- main.py
+++ main.py
@@ -2,21 +2,30 @@ from typing import Dict
 import random
 import roles
 
+
 def setup():
     pass
+
 
 def day():
     pass
 
+
 def night():
-    pass
+    print("Le village s'endort")
+    print("La voyante se réveille")
+    players["voyante"][0].turn(get_all_players(players))
+    print("Les loups-garous se réveillent")
+
 
 def sideEvent():
     pass
 
+
 players = {
     "villageois": [roles.Villageois() for _ in range(3)],
-    "loups_garous": [roles.LoupGarou() for _ in range(3)]
+    "loups_garous": [roles.LoupGarou() for _ in range(3)],
+    "voyante": [roles.Voyante()]
 }
 random.choice(list(players.values()))[-1].set_player()
 
@@ -33,9 +42,4 @@ while game:
     night()
     sideEvent()
     day()
-    
-    print("Le village s'endort")
-    print("La voyante se réveille")
-    players["voyante"][0].turn(get_all_players(players))
-    print("Les loups-garous se réveillent")
     input("pass")

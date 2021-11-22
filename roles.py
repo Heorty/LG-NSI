d0397f934a98750786547265aaa45e5371f9ea02
serialkilaure <94844133+serialkilaure@users.noreply.github.com>
2021-11-22 14:45:00 +0100

    section noms

--- roles.py
+++ roles.py
@@ -1,6 +1,18 @@
+import csv
+import random
+
+f= open ("./prenom.csv")
+csvfile = csv.reader(f)
+prenoms = []
+
+for row in csvfile:
+    prenoms.append(row[0].split(";")[0].split()[0])
+
+
 class Villageois:
     def __init__(self):
         self.type = "bot"
+        self.name = random.choice(prenoms)
 
     def set_player(self):
         self.type = "player"
@@ -15,3 +27,6 @@ class Villageois:
 class LoupGarou(Villageois):
     def turn(self, all_players):
         pass
+
+a = Villageois()
+print(a.name)

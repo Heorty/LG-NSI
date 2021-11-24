8ab5b6ba30c7aaea1aa0cdd503b2aa0a8123c4bd
serialkilaure <94844133+serialkilaure@users.noreply.github.com>
2021-11-24 08:56:17 +0100

    dialogue

--- lore.py
+++ lore.py
@@ -1,7 +1,21 @@
+def debut():
+    print("il fait froid, les loups-garou rodent dans la nature. le village s'endort malgrès cette insécurité en cette nuit de pleine Lune d'hiver")
 
-class Lore:
-    def __init__(self) -> None:
-        pass
+def voyante(dodo: bool):
+    if dodo:
+        print("La voyante, épuisée, s'endore")
+    else: 
+        print('la voyante se réveille')
 
-    def voyante(self):
-        print('la ')
+def loup_garoup(dodo: bool):
+    if dodo:
+        print('les loups-garous repus se rendorment et rêvent de prochaine victimes')
+    else:
+        print('les loups-garou se réveillent')
+
+def sorcière(dodo: bool):
+    if dodo:
+        print('la sorcière, après avoir épuisé ses potions, se rendort')
+    else:
+        print("la sorcière se réveille en apprenant la mort d'un villageois")
+

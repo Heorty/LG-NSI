097a0412c44667c26db1f9316ac04736ee5f2e21
Julien THILLARD <54775010+supersurviveur@users.noreply.github.com>
2021-11-24 08:44:03 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
 from typing import Dict
 import random
-
-import lore
 import roles
-
+import lore
 
 
 players = {
     "villageois": [roles.Villageois() for _ in range(3)],
     "loups_garous": [roles.LoupGarou() for _ in range(3)],
     "voyante": [roles.Voyante()]
-
 }
 random.choice(list(players.values()))[-1].set_player()
 
@@ -24,9 +21,17 @@ game = True
 
 while game:
 
+    print("Le village s'endort")
+    print("La voyante se réveille")
     players["voyante"][0].turn(get_all_players(players))
+
     print("Les loups-garous se réveillent")
     response = []
     for loup_garou in players["loups_garous"]:
         response.append(loup_garou.turn(get_all_players(players), response))
+    i = 0
+    for player in get_all_players(players):
+        if player.alive and not isinstance(player, roles.LoupGarou) and i == response[-1]:
+            killed = player
+
     input("pass")

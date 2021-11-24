6e3c3370861976a299be42ff4655f7991a662861
serialkilaure <94844133+serialkilaure@users.noreply.github.com>
2021-11-24 09:20:08 +0100

    update dia

--- lore.py
+++ lore.py
@@ -1,24 +1,25 @@
 import random
 
-def debutjour():
-    debphrase = ["il fait froid, les loups-garou rodent dans la nature. le village s'endort malgrès cette insécurité en cette nuit de pleine Lune d'hiver",
-                 "apres cette journée epuisante le village se rendort",
-                 "les supçons commence à se faire, mais le village, fatigué se rendort..."]
-    return random.choice(debphrase)
+
+def finjour():
+    jourphrase = ["il fait froid, les loups-garou rodent dans la nature. le village s'endort malgrès cette insécurité en cette nuit de pleine Lune d'hiver",
+                  "apres cette journée epuisante le village se rendort",
+                  "les supçons commence à se faire, mais le village, fatigué se rendort..."]
+    return random.choice(jourphrase)
 
 
 def voyante(dodo: bool):
     if dodo:
-        print("La voyante, épuisée, s'endore")
+        print("La voyante, épuisée, s'endort")
     else:
         print('la voyante se réveille')
 
 
 def loup_garoup(dodo: bool):
     if dodo:
-        print('les loups-garous repus se rendorment et rêvent de prochaine victimes')
+        print('ayant choisis leur victimes, les loups-garous repus se rendorment et rêvent de prochaine victimes')
     else:
-        print('les loups-garou se réveillent')
+        print('les loups-garou se réveillent affammés de choisir leur nouvelle victime pour leur repas')
 
 
 def sorcière(dodo: bool):
@@ -26,3 +27,9 @@ def sorcière(dodo: bool):
         print('la sorcière, après avoir épuisé ses potions, se rendort')
     else:
         print("la sorcière se réveille en apprenant la mort d'un villageois")
+
+
+def finnuit():
+    phanuit = ["le soleil se lève et le village se réveil",
+               "en cette nuit plutôt mouvementé, le village se réveil"]
+    return random.choice(phanuit)

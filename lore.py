b1cf91a34bd68be1f8f564fe49d75dee82812055
serialkilaure <94844133+serialkilaure@users.noreply.github.com>
2021-11-24 09:10:11 +0100

    dialogue 2

--- lore.py
+++ lore.py
@@ -1,11 +1,18 @@
-def debut():
-    print("il fait froid, les loups-garou rodent dans la nature. le village s'endort malgrès cette insécurité en cette nuit de pleine Lune d'hiver")
+import random
+
+def debutjour():
+    debphrase = ["il fait froid, les loups-garou rodent dans la nature. le village s'endort malgrès cette insécurité en cette nuit de pleine Lune d'hiver",
+                 "apres cette journée epuisante le village se rendort",
+                 "les supçons commence à se faire, mais le village, fatigué se rendort..."]
+    return random.choice(debphrase)
+
 
 def voyante(dodo: bool):
     if dodo:
         print("La voyante, épuisée, s'endore")
-    else: 
+    else:
         print('la voyante se réveille')
+
 
 def loup_garoup(dodo: bool):
     if dodo:
@@ -13,9 +20,9 @@ def loup_garoup(dodo: bool):
     else:
         print('les loups-garou se réveillent')
 
+
 def sorcière(dodo: bool):
     if dodo:
         print('la sorcière, après avoir épuisé ses potions, se rendort')
     else:
         print("la sorcière se réveille en apprenant la mort d'un villageois")
-
